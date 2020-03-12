f12b188c0f67323a46e496920854630dd9bc4a8c
felberto <tobias.felber97@gmail.com>
2020-03-12 23:11:22 +0100

    reworked join and save users and rooms

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const http = require('http').createServer(express);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 8000;
+const fs = require('fs');
+
+let rooms = [];
 
 io.origins('*:*'); // for latest version
 io.on('connection', function (socket) {
@@ -10,27 +13,38 @@ io.on('connection', function (socket) {
 
     socket.on('createRoom', (roomName, userName) => {
         socket.username = userName;
+        this.rooms.push({name: roomName, users: [socket.id], variables: {}});
         socket.join(roomName);
         console.log('created room ' + roomName);
+        console.log(this.rooms)
     });
 
     socket.on('joinRoom', (roomName, userName) => {
+        console.log(socket.id);
         socket.username = userName;
+        this.rooms.forEach(room => {
+            if (room.name === roomName) {
+                room.users.push(socket.id);
+            }
+        });
         socket.join(roomName);
         console.log('joined room ' + roomName);
+        console.log(this.rooms);
     });
 
     socket.on('getAllRooms', () => {
-        let availableRooms = [];
-        let rooms = io.sockets.adapter.rooms;
-        if (rooms) {
-            for (let room in rooms) {
-                if (!rooms[room].hasOwnProperty(room)) {
-                    availableRooms.push({name: room});
-                }
-            }
-        }
-        socket.emit('responseAllRooms', availableRooms);
+        socket.emit('responseAllRooms', this.rooms);
+    });
+
+    socket.on('startGame', () => {
+        let rawData = fs.readFileSync('cards.json');
+        let cards = JSON.parse(rawData);
+        console.log(cards);
+
+        //Store deck in client id => https://stackoverflow.com/questions/15876616/save-data-on-socket-in-socket-io
+        //Or over multiple sockets for all users
+        socket.deck = shuffle(cards);
+        console.log(socket.deck);
     });
 
     socket.on("disconnect", () => {
@@ -39,6 +53,23 @@ io.on('connection', function (socket) {
 });
 
 io.listen(8001);
+
+function shuffle(array) {
+    let ctr = array.length, temp, index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+        ctr--;
+        // And swap the last element with it
+        temp = array[ctr];
+        array[ctr] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
 
 // define a simple route
 app.get('/api', (req, res) => {
