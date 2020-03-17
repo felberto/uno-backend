17eb8c8dadc03673d41779867f586340c0f53cd8
felberto <tobias.felber97@gmail.com>
2020-03-17 13:17:01 +0100

    broadcast room data and start game

--- src/server.js
+++ src/server.js
@@ -13,7 +13,12 @@ io.on('connection', function (socket) {
 
     socket.on('createRoom', (roomName, userName) => {
         socket.username = userName;
-        this.rooms.push({name: roomName, playing: false, users: [{user: socket.id, cards: []}], deck: []});
+        this.rooms.push({
+            name: roomName,
+            playing: false,
+            users: [{user: socket.id, username: userName, cards: []}],
+            deck: []
+        });
         socket.join(roomName);
         console.log('created room ' + roomName);
     });
@@ -22,35 +27,56 @@ io.on('connection', function (socket) {
         socket.username = userName;
         this.rooms.forEach(room => {
             if (room.name === roomName) {
-                room.users.push({user: socket.id, cards: []});
+                room.users.push({user: socket.id, username: userName, cards: []});
             }
         });
         socket.join(roomName);
         console.log('joined room ' + roomName);
     });
 
+    socket.on('getRoomData', () => {
+        for (let i = 0; i < this.rooms.length; ++i) {
+            for (let y = 0; y < this.rooms[i].users.length; ++y) {
+                if (this.rooms[i].users[y].user === socket.id) {
+                    socket.emit('roomData', this.rooms[i]);
+                }
+            }
+        }
+    });
+
     socket.on('getAllRooms', () => {
         let availableRooms = [];
 
-        for (let index = 0; index < this.rooms.length; ++index) {
-            if (!this.rooms[index].playing) {
-                availableRooms.push(this.rooms[index]);
+        for (let i = 0; i < this.rooms.length; ++i) {
+            if (!this.rooms[i].playing) {
+                availableRooms.push(this.rooms[i]);
             }
         }
         socket.emit('responseAllRooms', availableRooms);
     });
 
-    socket.on('startGame', () => {
-
-
-        let rawData = fs.readFileSync('cards.json');
-        let cards = JSON.parse(rawData);
-        console.log(cards);
-
-        //Store deck in client id => https://stackoverflow.com/questions/15876616/save-data-on-socket-in-socket-io
-        //Or over multiple sockets for all users
-        socket.deck = shuffle(cards);
-        console.log(socket.deck);
+    socket.on('startGame', (room) => {
+        fs.readFile('./resources/cards.json', 'utf8', (err, jsonString) => {
+            if (err) {
+                console.log("File read failed:", err);
+                return
+            }
+            let cards = JSON.parse(jsonString);
+            for (let i = 0; i < this.rooms.length; ++i) {
+                if (this.rooms[i].name === room) {
+                    let shuffled = shuffle(cards.cards);
+                    for (let y = 0; y < this.rooms[i].users.length; ++y) {
+                        let count = 7;
+                        while (count !== 0) {
+                            this.rooms[i].users[y].cards.push(shuffled.shift());
+                            --count;
+                        }
+                    }
+                    this.rooms[i].deck.push(shuffled);
+                    io.to(room).emit('roomData', this.rooms[i]);
+                }
+            }
+        });
     });
 
     socket.on("disconnect", () => {
