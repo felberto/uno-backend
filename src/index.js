982c73ce2dd53460373d038aa922b1ed2265aa69
felberto <tobias.felber97@gmail.com>
2020-04-11 22:21:45 +0200

    fix origin

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Set CORS on express
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://uno-client.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
