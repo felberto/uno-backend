baf1ba8645f97af57f1ce87b44d08393a89e32a9
Tobias Felber <tobias.felber97@gmail.com>
2020-06-01 16:02:39 +0200

    removed tests and test stage for build

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../src/server');
-
-describe('Sample Test', () => {
-    test('should test that true === true', () => {
-        expect(true).toBe(true)
-    })
-});
